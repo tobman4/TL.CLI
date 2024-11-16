354a16ab762557548f0278d398dcd05e8db8c49a
tl <tl@home>
2024-11-16 23:27:52 +0100

    test test

--- src/TL.Test/Say.cs
+++ src/TL.Test/Say.cs
@@ -2,9 +2,19 @@ using TL.CLI.Attributes;
 
 public class Say {
 
+  [Argument("count")]
+  public int Count { get; set; } = 1;
+
   [Command("hello")]
-  public void Hello([Option("--name")]string name = "Tom") {
-    Console.WriteLine($"Hello {name}");
+  public void Hello([Argument("name", "Who to say hello to")]string name = "Tom") {
+    for(int i = 0; i < Count; i++)
+      Console.WriteLine($"Hello {name}");
+  }
+  
+  [Command("bye")]
+  public void Bye() {
+    for(int i = 0; i < Count; i++)
+      Console.WriteLine("Bye :,(");
   }
 
 }
